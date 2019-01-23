e2d672420259c18e6e3e0594aba8c1c797e886cc
kristenkwong <kristenkwong.wy@hotmail.com>
2019-01-23 00:37:42 -0800

    multiline comments and more skeleton fleshing

--- CSftp.java
+++ CSftp.java
@@ -11,123 +11,201 @@ import java.net.*;
 //
 
 
-public class CSftp
-{
+public class CSftp {
+
     static final int MAX_LEN = 255;
     static final int ARG_CNT = 2;
+    private static final int FTP_PORT = 21;
+
+    static Socket socket;
+    static PrintWriter out;
+    static BufferedReader in;
 
     private static void user(String param) {
-        // merp
+        sendCommand("USER " + param);
+        handleResponse();
     }
 
     private static void pw(String param) {
-        // merp
+        sendCommand("PASS " + param);
+        handleResponse();
+    }
+
+    private static void quit() {
+        sendCommand("QUIT");
+        handleResponse();
     }
 
     private static void get(String param) {
-
     }
 
     private static void features() {
-        //fd
+        sendCommand("FEAT");
+        handleResponse();
     }
 
     private static void cd(String param) {
-
+        sendCommand("CWD " + param);
+        handleResponse();
     }
 
     private static void dir() {
-
     }
 
-    public static void main(String [] args)
-    {
-	byte cmdString[] = new byte[MAX_LEN];
+    private static void establishControlConnection(String hostName, int portNumber) {
+        try {
+            socket = new Socket(hostName, portNumber);
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            handleResponse();
+        } catch (IOException exception) {
+            System.err.println(String.format("0xFFFC Control connection to %s on port %i failed to open.", hostName, portNumber));
+        }
+        return;
+    }
 
-	// Get command line arguments and connected to FTP
-	// If the arguments are invalid or there aren't enough of them
-        // then exit.
+    private static void handleResponse() {
+        try {
+            String fromServer;
+            fromServer = in.readLine();
 
-	if (args.length > ARG_CNT || args.length == 0) {
-	    System.out.print("Usage: cmd ServerAddress ServerPort\n");
-	    return;
-    } 
-    
-    String hostName = args[0];
-    // if no port number, default to 21
-    int portNumber = (args.length == 2) ? Integer.parseInt(args[1]) : 21;
+            // if response is multi-line, it'll start with response code followed
+            // immediately by a hyphen (ie. 211-Features)
+            String responseCode = fromServer.split(" ")[0];
+            int line_num = 0;
 
-	try (
-        Socket kkSocket = new Socket(hostName, portNumber);
-        PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(
-            new InputStreamReader(kkSocket.getInputStream()));
-    ) {
-        BufferedReader stdIn =
-                new BufferedReader(new InputStreamReader(System.in));
-        String fromServer;
-        String fromUser;
+            if (responseCode.contains("-")) {
+                // response contains multiple lines
+                responseCode = responseCode.split("-")[0];
+                boolean exit_flag = false;
 
-        while (true) {
-
-            /*fromServer = in.readLine();
-            if (fromServer != null) {
-                System.out.println("--> " + fromServer);
-                if (fromServer.startsWith("221"))
-                    break;
-            }*/
-
-            System.out.print("csftp> ");
-
-            fromUser = stdIn.readLine();
-            if (fromUser.equals("") || fromUser.startsWith("#"))
-                continue;
-
-            else if (fromUser != null) {
-                System.out.println("--> " + fromUser);
-                String[] commands = fromUser.split(" ");
-
-                String param = null;
-                if (commands[0].equals("user") || commands[0].equals("pw")
-                || commands[0].equals("get") || commands[0].equals("cd")) {
-                    // do something with params
-                    // check arg number
-                    // TODO
-                    param = commands[1];
+                while (true) {
+                    System.out.println("<-- " + fromServer);
+                    if (line_num != 0 && fromServer.startsWith(responseCode))
+                        break;
+                    else if (fromServer.startsWith("221"))
+                        exit_flag = true;
+                    fromServer = in.readLine();
+                    line_num++;
                 }
+                if (exit_flag) // if the program has been flagged to exit
+                    System.exit(0);
 
-                switch(commands[0]) {
-                    case "user":
-                        user(param);
-                        // out.print()
-                        break;
-                    case "pw":
-                        pw(param);
-                        break;
-                    case "quit":
-                        out.println("quit");
-                        break;
-                    case "get":
-                        get(param);
-                        break;
-                    case "features":
-                        features();
-                        break;
-                    case "cd":
-                        cd(param);
-                        break;
-                    case "dir":
-                        dir();
-                        break;
-                    default:
-                        System.out.println("invalid command");
-                        break;
+            } else {
+                // response is single line
+                System.out.println("<-- " + fromServer);
+                if (fromServer.startsWith("221")) // connection closed
+                    System.exit(0);
+            }
+            
+        } catch (IOException exception) {
+            System.err.println("error handling response");
+        } 
+    }
+
+    private static void sendCommand(String command) {
+        out.println(command + "\r\n");
+        System.out.println("--> " + command);
+    }
+
+    public static void main(String [] args) {
+        byte cmdString[] = new byte[MAX_LEN];
+
+        // Get command line arguments and connected to FTP
+        // If the arguments are invalid or there aren't enough of them
+            // then exit.
+        if (args.length > ARG_CNT || args.length == 0) {
+            System.out.print("Usage: cmd ServerAddress ServerPort\n");
+            return;
+        } 
+        
+        String hostName = args[0];
+        // if no port number, default to 21
+        int portNumber = 21;
+        try {
+            portNumber = (args.length == 2) ? Integer.parseInt(args[1]) : FTP_PORT;
+        } catch (NumberFormatException error) {
+            System.out.println("0xFFFF Processing error. Port number isn't a number.");
+            System.exit(0);
+        }
+        
+        try {
+            int len;
+            establishControlConnection(hostName, portNumber);
+
+            while (true) {
+                System.out.print("csftp> ");
+                len = System.in.read(cmdString);
+                String command = new String(cmdString).trim();
+
+                // if empty string or starts with #, silently ignore and display prompt
+                if (len <= 0 || command.equals("") || command.startsWith("#"))
+                    continue;
+            
+                else {
+                    // System.out.println("--> " + fromUser);
+                    String[] commands = command.split(" ");
+
+                    String param = null;
+                    
+                    switch(commands[0]) {
+                        case "user":
+                            if (commands.length != 2) {
+                                System.err.println("0x002 Incorrect number of arguments.");
+                                break;
+                            }
+                            user(param); break;
+                        case "pw":
+                            if (commands.length != 2) {
+                                System.err.println("0x002 Incorrect number of arguments.");
+                                break;
+                            }
+                            pw(param); break;
+                        case "quit":
+                            if (commands.length != 1) {
+                                System.err.println("0x002 Incorrect number of arguments.");
+                                break;
+                            }
+                            quit();
+                            break;
+                        case "get":
+                            if (commands.length != 2) {
+                                System.err.println("0x002 Incorrect number of arguments.");
+                                break;
+                            }
+                            get(param);
+                            break;
+                        case "features":
+                            if (commands.length != 1) {
+                                System.err.println("0x002 Incorrect number of arguments.");
+                                break;
+                            }
+                            features();
+                            break;
+                        case "cd":
+                            if (commands.length != 2) {
+                                System.err.println("0x002 Incorrect number of arguments.");
+                                break;
+                            }
+                            cd(param);
+                            break;
+                        case "dir":
+                            if (commands.length != 1) {
+                                System.err.println("0x002 Incorrect number of arguments.");
+                                break;
+                            }
+                            dir();
+                            break;
+                        default:
+                            System.out.println("900 Invalid command.");
+                            break;
+                    }
+
+                    cmdString = new byte[MAX_LEN]; // clear byte array after cmd
                 }
             }
+        } catch (IOException exception) {
+            System.err.println("998 Input error while reading commands, terminating.");
         }
-
-	} catch (IOException exception) {
-	    System.err.println("998 Input error while reading commands, terminating.");
-	}
     }
 }
