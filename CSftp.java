4dadc49b4889ad779e4525014083ed177caffeec
kristenkwong <kristenkwong.wy@hotmail.com>
2019-01-23 18:07:49 -0800

    dir

--- CSftp.java
+++ CSftp.java
@@ -37,35 +37,7 @@ public class CSftp {
     }
 
     private static void get(String param) {
-        /* open a new data connection */
-        sendCommand("PASV");
-        String[] hostNumbers = handlePassiveResponse();
-        if (hostNumbers == null)
-            return;
-        String hostName = getIpAddress(hostNumbers);
-        int portNumber = getPortNumber(hostNumbers);
-        System.out.println(hostName);
-        System.out.println(portNumber);
-
-        try (
-            Socket kkSocket = new Socket(hostName, portNumber);
-            PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(
-                new InputStreamReader(kkSocket.getInputStream()));
-        ) {
-            BufferedReader stdIn =
-                new BufferedReader(new InputStreamReader(System.in));
-            String fromServer;
-
-            
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host " + hostName);
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " +
-                hostName);
-            System.exit(1);
-        }
+        
     }
 
     private static void features() {
@@ -79,7 +51,39 @@ public class CSftp {
     }
 
     private static void dir() {
+        sendCommand("PASV");
+        String[] hostNumbers = handlePassiveResponse();
+        if (hostNumbers == null)
+            return;
+        String hostName = getIpAddress(hostNumbers);
+        int portNumber = getPortNumber(hostNumbers);
 
+        try (
+            Socket kkSocket = new Socket(hostName, portNumber);
+            PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(kkSocket.getInputStream()));
+        ) {
+            BufferedReader stdIn =
+                new BufferedReader(new InputStreamReader(System.in));
+            String fromServer;
+
+            sendCommand("LIST");
+            handleResponse();
+
+            while ((fromServer = in.readLine()) != null) {
+                System.out.println("<-- " + fromServer);
+            }
+            
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                hostName);
+            System.exit(1);
+        }
+        handleResponse();
     }
 
     private static void establishControlConnection(String hostName, int portNumber) {
