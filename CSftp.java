94cc29bdf441c571a3d236766b94bedc3a0f8767
kristenkwong <kristenkwong.wy@hotmail.com>
2019-01-23 17:17:31 -0800

    data connection

--- CSftp.java
+++ CSftp.java
@@ -37,6 +37,26 @@ public class CSftp {
     }
 
     private static void get(String param) {
+
+        /* open a new data connection */
+        try (
+            Socket kkSocket = new Socket(hostName, portNumber);
+            PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(kkSocket.getInputStream()));
+        ) {
+            BufferedReader stdIn =
+                new BufferedReader(new InputStreamReader(System.in));
+            String fromServer;
+            String fromUser;
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                hostName);
+            System.exit(1);
+        }
     }
 
     private static void features() {
@@ -50,6 +70,7 @@ public class CSftp {
     }
 
     private static void dir() {
+
     }
 
     private static void establishControlConnection(String hostName, int portNumber) {
